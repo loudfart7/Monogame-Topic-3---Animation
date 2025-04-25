f149d133ff77aaf4e9dc17257a6651f05e533af0
Chloe Vandeworp <339639510@GoTVDSB.ca>
2025-04-25 14:24:16 -0400

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Monogame_Topic_3___Animation
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Random generator = new Random();
+
+        Rectangle window;
+
+        Texture2D tribbleBrownTexture;
+        Rectangle tribbleBrownRect;
+        Vector2 tribbleBrownSpeed;
+
+        Texture2D tribbleCreamTexture;
+        Rectangle tribbleCreamRect;
+        Vector2 tribbleCreamSpeed;
+
+        Texture2D tribbleGreyTexture;
+        Rectangle tribbleGreyRect;
+        Vector2 tribbleGreySpeed;
+
+        Texture2D tribbleOrangeTexture;
+        Rectangle tribbleOrangeRect;
+        Vector2 tribbleOrangeSpeed;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            window = new Rectangle(0,0,800,600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            tribbleBrownRect = new Rectangle(300, 10, 100, 100);
+            tribbleBrownSpeed = new Vector2(2, 2);
+
+            tribbleGreyRect = new Rectangle(300,10,100,100);
+            tribbleGreySpeed = new Vector2(2, 2);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            tribbleGreyTexture = Content.Load<Texture2D>("tribbleGrey");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            tribbleGreyRect.X += (int)tribbleGreySpeed.X;
+            if (tribbleGreyRect.Right >= window.Width || tribbleGreyRect.Left <= 0)
+                tribbleGreySpeed.X *= -1;
+
+            tribbleGreyRect.Y += (int)tribbleGreySpeed.Y;
+            if (tribbleGreyRect.Bottom >= window.Height || tribbleGreyRect.Top <= 0)
+                tribbleGreySpeed.Y *= -1;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(tribbleGreyTexture, tribbleGreyRect, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame_Topic_3___Animation.Game1();
+game.Run();
